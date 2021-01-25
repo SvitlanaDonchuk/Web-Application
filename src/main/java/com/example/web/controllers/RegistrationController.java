8cc6f0e69fcb50886006444db875274e97391f42
SvitlanaDonchuk <sveta.donchuk@gmail.com>
2021-01-25 17:46:22 +0200

    Added password confirmation

--- src/main/java/com/example/web/controllers/RegistrationController.java
+++ src/main/java/com/example/web/controllers/RegistrationController.java
@@ -5,10 +5,12 @@ import com.example.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -25,10 +27,23 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user,
-                          BindingResult bindingResult, Model model){
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @Valid User user,
+            BindingResult bindingResult, Model model
+    ){
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if(isConfirmEmpty){
+            model.addAttribute("password2Error", "Password confirmation can not be empty");
+        }
 
-        if(bindingResult.hasErrors()){
+        if(user.getPassword() != null && !user.getPassword().equals(passwordConfirm)){
+            model.addAttribute("passwordError", "Passwords are different!");
+
+            return "registration";
+        }
+
+        if(isConfirmEmpty || bindingResult.hasErrors()){
             Map<String, String> errors = UtilsController.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
@@ -50,8 +65,10 @@ public class RegistrationController {
         boolean isActivated = userService.activateUser(code);
 
         if (isActivated) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "User successfully activated");
         } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code is not found!");
         }
 
--- src/main/java/com/example/web/entity/User.java
+++ src/main/java/com/example/web/entity/User.java
@@ -16,6 +16,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
